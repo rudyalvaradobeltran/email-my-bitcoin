af40ad02e605db661f86e89c66a44b06cebc2b51
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2024-07-08 23:36:09 -0400

    SES implementation v1

--- deploy/lib/lambda-bitcoin-stack.ts
+++ deploy/lib/lambda-bitcoin-stack.ts
@@ -1,33 +1,63 @@
-import {
-  Duration,
-  Stack,
-  StackProps
-} from "aws-cdk-lib";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
-import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { Rule, Schedule } from "aws-cdk-lib/aws-events";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 import { join } from "path";
 import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
+import {
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 
 export class LambdaBitcoinStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
+    const role = new Role(this, "SesSenderRole", {
+      roleName: "SesSenderRole",
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonSESFullAccess")
+    );
+
+    role.addToPrincipalPolicy(
+      new PolicyStatement({
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: ["*"]
+      })
+    );
+    
     const lambdaBitcoin = new NodejsFunction(this, "LambdaBitcoinFunction", {
       runtime: Runtime.NODEJS_18_X,
       handler: "handler",
-      entry: join(__dirname, "..", "..", "src", "LambdaBitcoin", "build/index.js"),
+      entry: join(
+        __dirname,
+        "..",
+        "..",
+        "src",
+        "LambdaBitcoin",
+        "build/index.js"
+      ),
       tracing: Tracing.ACTIVE,
       logRetention: RetentionDays.SIX_MONTHS,
       timeout: Duration.seconds(60),
       environment: {},
+      role: role
     });
 
-    new Rule(this, 'rule', {
+    new Rule(this, "rule", {
       schedule: Schedule.rate(Duration.minutes(5)),
-      targets: [new LambdaFunction(lambdaBitcoin)]
+      targets: [new LambdaFunction(lambdaBitcoin)],
     });
   }
 }
--- src/LambdaBitcoin/src/index.ts
+++ src/LambdaBitcoin/src/index.ts
@@ -1,13 +1,35 @@
 import { Logger, injectLambdaContext } from "@aws-lambda-powertools/logger";
+import { SESv2Client, SendEmailCommand, SendEmailCommandInput } from "@aws-sdk/client-sesv2"
 import middy from "@middy/core";
 import * as axios from "./services/axios";
 
 const logger = new Logger({ serviceName: "lambda-bitcoin" });
+let client: SESv2Client;
 
-const lambdaHandler = async (): Promise<void> => {
+const lambdaHandler = async (event: any): Promise<void> => {
   try {
+    if (!client) client = new SESv2Client({});
     const response = await axios.default.get('bitcoin');
-    logger.info(`Current bitcoin price at ${new Date().toLocaleString()}: ${response.data.data.priceUsd.split(".")[0]}`);
+    const input: SendEmailCommandInput = {
+      FromEmailAddress: 'Rudy Alvarado <rudy.alvarado.b@gmail.com>',
+      Destination: { ToAddresses: [ 'rudy.alvarado.b@gmail.com' ] },
+      EmailTags: [{ Name: 'type', Value: 'bitcoin' }],
+      ReplyToAddresses: ['rudy.alvarado.b@gmail.com'],
+      Content: {
+        Simple: {
+          Subject: { Data: "Today's Bitcoin Price!" },
+          Body: {
+            Html: {
+              Charset: 'UTF-8',
+              Data: `Current bitcoin price at ${new Date().toLocaleString()}: ${response.data.data.priceUsd.split(".")[0]}`
+            },
+            Text: { Data: 'TEST EMAIL' }
+          }
+        }
+      }
+    }
+    const command = new SendEmailCommand(input);
+    await client.send(command);
   } catch (err: any) {
     logger.error(err);
   }
