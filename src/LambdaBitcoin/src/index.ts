5a8ee7e681a6912bbff2eb11b8c394fa177c40ed
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2024-07-20 21:02:10 -0400

    Unused vars/method

--- src/LambdaBitcoin/src/index.ts
+++ src/LambdaBitcoin/src/index.ts
@@ -2,13 +2,13 @@ import { Logger, injectLambdaContext } from "@aws-lambda-powertools/logger";
 import { SESv2Client, SendEmailCommand, SendEmailCommandInput } from "@aws-sdk/client-sesv2"
 import middy from "@middy/core";
 import * as axios from "./services/axios";
-import { getSecret, cleanSecret } from "./services/secret-manager";
+import { getSecret } from "./services/secret-manager";
 import template from "./email/template";
 
 const logger = new Logger({ serviceName: "lambda-bitcoin" });
 let client: SESv2Client;
 
-const lambdaHandler = async (event: any): Promise<void> => {
+const lambdaHandler = async (): Promise<void> => {
   try {
     if (!client) client = new SESv2Client({});
     let senderemail: string = await getSecret(process.env.senderEmailArn as string);
